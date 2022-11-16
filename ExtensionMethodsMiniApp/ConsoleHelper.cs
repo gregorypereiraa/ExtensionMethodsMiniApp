18c685aaf74a14e04a73ff153b6aa16f3b02243b
gregory <gregory.pereiraa@gmail.com>
2022-11-16 19:20:08 +1100

    Extensions

---
+++ ExtensionMethodsMiniApp/ConsoleHelper.cs
@@ -0,0 +1,28 @@
+﻿using Spectre.Console;
+
+public static class ConsoleHelper
+{
+    public static int RequestInt(this string message)
+    {
+        return message.RequestInt(false);
+    }
+
+    public static int RequestInt(this string message, int min, int max)
+    {
+        return message.RequestInt(true, min, max);
+    }
+
+    private static int RequestInt(this string message, bool testRangeAge, int min = 0, int max = 0)
+    {
+        var output = 0;
+        if (testRangeAge)
+            do
+            {
+                output = AnsiConsole.Ask<int>(message);
+            } while ((output <= min) | (output > max));
+        else
+            output = AnsiConsole.Ask<int>(message);
+
+        return output;
+    }
+}
--- ExtensionMethodsMiniApp/Program.cs
+++ ExtensionMethodsMiniApp/Program.cs
@@ -1,3 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using Spectre.Console;
+
+var person = new PersonModel();
+person.FirstName = AnsiConsole.Ask<string>("What is your first name: ");
+person.LastName = AnsiConsole.Ask<string>("What is your Last name: ");
+person.Age = "What is your age: ".RequestInt(0, 120);
